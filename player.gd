b7cc41ce911840f4cfc1815247e53f6d56b4a39c
Danylo <107553788+qdanq@users.noreply.github.com>
2023-10-28 23:27:15 +0300

    Camera setup. Add dirlight to scene.

--- player.gd
+++ player.gd
@@ -1,32 +1,29 @@
-extends MeshInstance3D
+extends CharacterBody3D
 
-const ROT = 2
 const Gr = -20
-const SPEED = 120
+const SPEED = 100
 var vel = Vector3()
 
 func _physics_process(delta):
 	var dir = Vector3()
 	
 	if Input.is_action_pressed("Left"):
-		rotate_y(ROT*delta)
-			
+		dir.x = -1
 	if Input.is_action_pressed("Right"):
-		rotate_y(-ROT*delta)	
-
+		dir.x = 1	
 	if Input.is_action_pressed("Down"):
 		dir.z = 1
-
 	if Input.is_action_pressed("Up"):
 		dir.z = -1
 		
 	if dir:
 		dir *= SPEED * delta
-		dir = dir.rotated(Vector3(0,1,0), rotation.y)
-
+		
 	vel.x = dir.x
 	vel.z = dir.z
 	
-	vel.y = Gr * delta
+	vel.y += Gr * delta
 	
-	vel = move_and_slide(vel, Vector3(0, 1, 0))
+	move_and_slide()
+
+	
