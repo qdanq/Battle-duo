47753181a27792ae92e8e0e431053d52854d16b3
Danylo <107553788+qdanq@users.noreply.github.com>
2023-11-08 13:20:17 +0200

    Change gravity, speed and jump velocity.

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,11 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 15.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 13.0
+const JUMP_VELOCITY = 8.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 20.0
 @onready var neck := $Neck 
 @onready var camera := $Neck/Camera3D
 
