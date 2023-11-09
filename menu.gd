892e0c93ef9854b13c203b14785d7ff8be7cb151
Danylo <107553788+qdanq@users.noreply.github.com>
2023-11-09 13:54:41 +0200

    Make host and join.

--- menu.gd
+++ menu.gd
@@ -16,10 +16,14 @@ func _on_host_button_pressed():
 
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
 	
 	add_player(multiplayer.get_unique_id())
 func _on_join_button_pressed():
-	pass
+	main_menu.hide()
+
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
 	
 func add_player(peer_id):
 	var game = Game.instantiate()
