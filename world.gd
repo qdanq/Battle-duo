9d817a368681a8c5fa1dc1ca55a431f4b6c11e12
Danylo <107553788+qdanq@users.noreply.github.com>
2023-11-12 13:41:44 +0200

    Delete menu scene. Add player connect.

--- world.gd
+++ world.gd
@@ -11,18 +11,21 @@ func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 
-
 func _on_host_button_pressed():
 	main_menu.hide()
 	
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
 	
 	add_player(multiplayer.get_unique_id())
 
-
 func _on_join_button_pressed():
-	pass # Replace with function body.
+	main_menu.hide()
+	
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	
 	
 func add_player(peer_id):
 	var player = Player.instantiate()
