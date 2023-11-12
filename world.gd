892e251346982ae3f5ce67fd45d2623d417fa605
Danylo <107553788+qdanq@users.noreply.github.com>
2023-11-12 20:44:00 +0200

    Delete upnp func.

--- world.gd
+++ world.gd
@@ -24,7 +24,6 @@ func _on_host_button_pressed():
 	
 	add_player(multiplayer.get_unique_id())
 	
-	upnp_setup()
 
 func _on_join_button_pressed():
 	main_menu.hide()
@@ -52,19 +51,3 @@ func update_health_bar(health_value):
 func _on_multiplayer_spawner_spawned(node):
 	if node.is_multiplayer_authority():
 		node.health_changed.connect(update_health_bar)
-
-func upnp_setup():
-	var upnp = UPNP.new()
-	
-	var discover_result = upnp.discover()
-	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
-	"UPNP Discover Failed! Error %s" % discover_result)
-	
-	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
-	"UPNP Invalid Gateway!")
-	
-	var map_result = upnp.add_port_mapping(PORT)
-	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
-	"UPNP Port Mapping Failed! Error %s" % map_result)
-
-	print("Success! Join Address: %s" % upnp.query_external_address())
