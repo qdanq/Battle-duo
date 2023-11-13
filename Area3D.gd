500c30a0092894e29fb32ca0930c02201d46f4a0
Danylo <107553788+qdanq@users.noreply.github.com>
2023-11-13 12:32:03 +0200

    Add colision script to area3d.

--- Area3D.gd
+++ Area3D.gd
@@ -2,9 +2,7 @@ extends Area3D
 
 
 func _on_body_entered(body):
-	print("test1")
-	if body is Player:
-		print("test")
-
-
+	if body is CharacterBody3D:
+		print("TEST")
+		Player.receive_damage()
 
