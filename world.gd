5919a09028c168e7b7413c49a18cf3ccab94d54f
Danylo <107553788+qdanq@users.noreply.github.com>
2023-11-15 15:06:19 +0200

    Add player connect and disconnected func
    Comment old multiplayer code.

--- world.gd
+++ world.gd
@@ -12,52 +12,83 @@ const PORT = 6006
 var enet_peer = ENetMultiplayerPeer.new() # For future loby declare clear var and enet move to hostbutton
 
 
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
 
-func _on_host_button_pressed():
-	main_menu.hide()
-	hud.show()
-	
-	enet_peer.create_server(PORT)
-	multiplayer.multiplayer_peer = enet_peer
-	multiplayer.peer_connected.connect(add_player)
-	multiplayer.peer_disconnected.connect(remove_player)
-	
-	add_player(multiplayer.get_unique_id())
 
-	#enet_peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
-	
-	#multiplayer.set_multiplayer_peer(enet_peer)
 
-func _on_join_button_pressed():
-	main_menu.hide()
-	hud.show()
-	
-	enet_peer.create_client(address_entry.text, PORT)
-	multiplayer.multiplayer_peer = enet_peer
-	#enet_peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
-	#multiplayer.set_multiplayer_peer(enet_peer)
-	
-func add_player(peer_id):
-	var player = Player.instantiate()
-	player.position = Vector3(-255.6, 148.15, -25.796)
-	player.name = str(peer_id)
-	add_child(player)
-	
-	if player.is_multiplayer_authority():
-		player.health_changed.connect(update_health_bar)
-		
 
-func remove_player(peer_id):
-	var player = get_node_or_null(str(peer_id))
-	if player:
-		player.queue_free()
 
-func update_health_bar(health_value):
-	health_bar.value = health_value
 
-func _on_multiplayer_spawner_spawned(node):
-	if node.is_multiplayer_authority():
-		node.health_changed.connect(update_health_bar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#func _unhandled_input(event):
+#	if Input.is_action_just_pressed("quit"):
+#		get_tree().quit()
+#
+#func _on_host_button_pressed():
+#	main_menu.hide()
+#	hud.show()
+#
+#	enet_peer.create_server(PORT)
+#	multiplayer.multiplayer_peer = enet_peer
+#	multiplayer.peer_connected.connect(add_player)
+#	multiplayer.peer_disconnected.connect(remove_player)
+#
+#	add_player(multiplayer.get_unique_id())
+#
+#	#enet_peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+#
+#	#multiplayer.set_multiplayer_peer(enet_peer)
+#
+#func _on_join_button_pressed():
+#	main_menu.hide()
+#	hud.show()
+#
+#	enet_peer.create_client(address_entry.text, PORT)
+#	multiplayer.multiplayer_peer = enet_peer
+#	#enet_peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+#	#multiplayer.set_multiplayer_peer(enet_peer)
+#
+#func add_player(peer_id):
+#	var player = Player.instantiate()
+#	player.position = Vector3(-255.6, 148.15, -25.796)
+#	player.name = str(peer_id)
+#	add_child(player)
+#
+#	if player.is_multiplayer_authority():
+#		player.health_changed.connect(update_health_bar)
+#
+#
+#func remove_player(peer_id):
+#	var player = get_node_or_null(str(peer_id))
+#	if player:
+#		player.queue_free()
+#
+#func update_health_bar(health_value):
+#	health_bar.value = health_value
+#
+#func _on_multiplayer_spawner_spawned(node):
+#	if node.is_multiplayer_authority():
+#		node.health_changed.connect(update_health_bar)
+
+
+func _on_host_button_button_down():
+	pass # Replace with function body.
+
+
+func _on_join_button_button_down():
+	pass # Replace with function body.
+
+
+func _on_start_game_button_down():
+	pass # Replace with function body.
